d33b520a9b27caaaa5abd77b9a55cdcd65edce24
Janhavi Komurlekar <j.komurlekar@somaiya.edu>
2023-04-02 02:06:21 +0530

    new files related to predict added

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -4,7 +4,7 @@ import React,{useState} from 'react'
 // import Sidebar from '../components/Sidebar';
 import Navbar from '../components/Navbar';
 import Services from '../components/Services';
-import Predict from '../components/Predict';
+import Predict from '../components/predict';
 
 // import Footer from '../components/footer';
 
