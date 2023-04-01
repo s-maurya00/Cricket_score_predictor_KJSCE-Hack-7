505a9334a450ff55fe720c2e0ecaa47866868dcd
Sushil <mauryasushil045@gmail.com>
2023-04-01 13:29:07 +0530

    feat: added basic web scraper template

---
+++ Crickbuzz Scraper/score.py
@@ -0,0 +1,10 @@
+def get_match():
+    pass
+
+print("Enter crickbuzz score-card url for scraping:")
+print("Eg.: \"https://www.cricbuzz.com/live-cricket-scorecard/60035/ind-vs-aus-1st-odi-australia-tour-of-india-2023\"")
+url = input("Enter:\t")
+
+get_match(url)
+
+print("Done")
